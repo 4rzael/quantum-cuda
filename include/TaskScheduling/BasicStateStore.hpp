00e4016cc5b8a945154f9b282f4b43a61bef7d35
4rzael <maxime.agor23@gmail.com>
2018-08-12 20:11:34 +0200

    Added Basic StateStore

---
+++ include/TaskScheduling/BasicStateStore.hpp
@@ -0,0 +1,27 @@
+/**
+ * @Author: Maxime Agor (4rzael)
+ * @Date:   Sun Aug 12 2018
+ * @Email:  maxime.agor23@gmail.com
+ * @Project: CUDA-Based Simulator of Quantum Systems
+ * @Filename: BasicStateStore.hpp
+ * @Last modified by:   4rzael
+ * @Last modified time: Sun Aug 12 2018, 20:05:37
+ * @License: MIT License
+ */
+
+#pragma once
+#include <map>
+
+#include "TaskScheduling/IStateStore.hpp"
+
+namespace StateStore {
+    class BasicStateStore: public IStateStore { // TODO: store pointers ? IDK
+    public:
+        virtual bool storeState(StateId id, StateData const &state);
+        virtual bool deleteState(StateId id);
+        virtual StateData const &getState(StateId id);
+
+    private:
+        std::map<StateId, StateData> _map;
+    };
+}
---
+++ src/TaskScheduling/BasicStateStore.cpp
@@ -0,0 +1,28 @@
+/**
+ * @Author: Maxime Agor (4rzael)
+ * @Date:   Sun Aug 12 2018
+ * @Email:  maxime.agor23@gmail.com
+ * @Project: CUDA-Based Simulator of Quantum Systems
+ * @Filename: BasicStateStore.cpp
+ * @Last modified by:   4rzael
+ * @Last modified time: Sun Aug 12 2018, 20:05:42
+ * @License: MIT License
+ */
+
+#include "TaskScheduling/BasicStateStore.hpp"
+
+using namespace StateStore;
+
+bool BasicStateStore::storeState(StateId id, StateData const &state) {
+    _map[id] = state;
+    return true;
+}
+
+bool BasicStateStore::deleteState(StateId id) {
+    _map.erase(id);
+    return true;
+}
+
+StateData const &BasicStateStore::getState(StateId id) {
+    return _map.at(id);
+}
