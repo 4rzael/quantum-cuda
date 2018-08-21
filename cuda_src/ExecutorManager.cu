d55aa9162b8ca47ed5475a47bf4e26528b5912f6
l3ninj <julien.vial-detambel@epitech.eu>
2018-08-21 22:23:51 +0200

    basic argparse

--- cuda_src/ExecutorManager.cu
+++ cuda_src/ExecutorManager.cu
@@ -5,7 +5,7 @@
  * @Project: CUDA-Based Simulator of Quantum Systems
  * @Filename: ExecutorManager.cu
  * @Last modified by:   l3ninj
- * @Last modified time: 2018-08-21T11:02:43+02:00
+ * @Last modified time: 2018-08-21T22:22:44+02:00
  * @License: MIT License
  */
 
@@ -17,14 +17,18 @@
 
 ExecutorManager::ExecutorManager() {
   //m_executor = new GPUExecutor(QCUDA::GPUCriteria::HIGHER_COMPUTE_CAPABILITY);
-  try {
-    m_executor = new GPUExecutor(QCUDA::GPUCriteria::HIGHER_COMPUTE_CAPABILITY);
-  } catch(std::runtime_error e) {
-    LOG(Logger::WARNING, "Catched a runtime error at executor instantiation:"
-      << "\nstd::runtime_error:\n\t"
-      << e.what()
-      << "\nSwitching to naive CPU algebra executor."
-      << std::endl);
+  if (g_cpu_execution == false) {
+    try {
+      m_executor = new GPUExecutor(QCUDA::GPUCriteria::HIGHER_COMPUTE_CAPABILITY);
+    } catch(std::runtime_error e) {
+      LOG(Logger::WARNING, "Catched a runtime error at executor instantiation:"
+        << "\nstd::runtime_error:\n\t"
+        << e.what()
+        << "\nSwitching to naive CPU linear algebra executor."
+        << std::endl);
+      m_executor = new CPUExecutor();
+    }
+  } else {
     m_executor = new CPUExecutor();
   }
 }
